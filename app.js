8098d7430bc03f9a930a4de9e181e130886c232b
Ignacho2311 <candia316@gmail.com>
2023-08-17 19:02:41 -0400

    integracion proxy para APi Sportmonks

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors")
+const { createProxyMiddleware } = require("http-proxy-middleware"); // Importa la librería
 const conexionDB = require("./db/config");
 const authRouter = require("./routes/auth");
 const myAccountRouter = require("./routes/myaccount");
@@ -10,6 +11,17 @@ conexionDB()
 
 app.use(express.json())
 app.use(cors())
+
+app.use("/api/proxy", createProxyMiddleware({
+    target: "https://api.sportmonks.com/v3",
+    changeOrigin: true,
+    pathRewrite: {
+      "^/": "" // Elimina el prefijo /api/proxy de la ruta
+    },
+    headers: {
+      "Authorization": "5CmvZkaC2QfE3LM0OP8c1mX1uCQrN43nbHNuLSiltjWucCjLw1CBwSDsuHd4" // Agrega tu token de API aquí
+    }
+  }));
 
 
 app.use("/", express.static(__dirname + "/public"));
